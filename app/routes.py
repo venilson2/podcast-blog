c75905debaa52aa2be7cbb4fa85acb32b2b6519e
PhoenixBytee <venilson2009@hotmail.com>
2023-05-16 22:28:21 -0300

    refactor: separacao html para arquio base

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,14 @@
 from flask import Blueprint
 from app.controllers.default_controller import DefaultController
+import traceback
 
 default_bp = Blueprint('default', __name__)
 
+@default_bp.route('/home')
 @default_bp.route('/')
 def index():
-    return DefaultController.index()
+    try:
+        return DefaultController.index()
+    except Exception as e:
+        traceback.print_exc()
+        return "Error: An internal server error occurred."
